491f98fc297926249d4ca8b9eca24e7d14ef55c8
Jyoti-meme <jyoti.mala@memeinfotech.com>
2018-08-08 13:18:09 +0530

    typeid added inn] drug type

--- common/models/drug-type.js
+++ common/models/drug-type.js
@@ -1,5 +1,22 @@
 'use strict';
+// const Composer = require('../lib/composer.js');
+// const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+// const bizNetworkConnection = new BusinessNetworkConnection();
+// const cardName = "admin@mefy";
 
-module.exports = function(Drugtype) {
+module.exports = function (Drugtype) {
+
+    /** alter type and trim to get typeId */
+    Drugtype.beforeRemote('create', function (context, drugtype, next) {
+        let drugtypeId;
+        if (context.args.data.type.includes('')) {
+            drugtypeId = context.args.data.type.replace(/ +/g, "");
+        }
+        else {
+            drugtypeId = context.args.data.type;
+        }
+        context.args.data.typeId = drugtypeId.toLowerCase();
+        next();
+    });
 
 };
--- common/models/madicine-master.js
+++ common/models/madicine-master.js
@@ -1,5 +1,41 @@
 'use strict';
+const Composer = require('../lib/composer.js');
+const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+const bizNetworkConnection = new BusinessNetworkConnection();
+const cardName = "admin@mefy";
+
 
 module.exports = function(Medicinemaster) {
+
+
+    Medicinemaster.observe('loaded', function (context, next) {
+        console.log(context.data)
+        let drugtype;
+        drugtype = context.data.drugtype.split("#")[1];
+      console.log('drugtype'+drugtype)
+        bizNetworkConnection.connect(cardName)
+          .then((result) => {
+            bizNetworkConnection.getAssetRegistry('io.mefy.pharmacy.DrugType')
+              .then((assetRegistry) => {
+                  console.log('asset registry',assetRegistry)
+                return assetRegistry.get(context.data.drugtype);
+              }).then(function (drug) {
+                  console.log('drug',drug)
+                // pharmacy.address
+                let drugdata = {
+                  "$class": "io.mefy.pharmacy.DrugType",
+                  "typeId":drug.typeId,
+                  "type":drug.type,
+                 "description":drug.description
+                }
+                context.data.drugtype = drugdata;
+                next();
+              });
+          }).catch((error) => {
+            next();
+            console.log('err'+error);
+          });
+      });
+    
 
 };
