9db276b3ec634cc6f0308e6c16e67010622fe15e
Jyoti-meme <jyoti.mala@memeinfotech.com>
2018-08-09 09:51:05 +0530

    changes in medicine master

--- common/models/madicine-master.js
+++ common/models/madicine-master.js
@@ -5,33 +5,37 @@ const bizNetworkConnection = new BusinessNetworkConnection();
 const cardName = "admin@mefy";
 
 
-module.exports = function(Medicinemaster) {
+module.exports = function (Medicinemaster) {
 
 
-    Medicinemaster.observe('loaded', function (context, next) {
-        let drugtypeId;
-        drugtypeId = context.data.drugtype.split("#")[1];
-        bizNetworkConnection.connect(cardName)
-          .then((result) => {
-            bizNetworkConnection.getAssetRegistry('io.mefy.pharmacy.DrugType')
-              .then((assetRegistry) => {
-                  console.log('asset registry',assetRegistry)
-                return assetRegistry.get(drugtypeId);
-              }).then(function (drug) {
-                let drugdata = {
-                  "$class": "io.mefy.pharmacy.DrugType",
-                  "typeId":drug.typeId,
-                  "type":drug.type,
-                 "description":drug.description
-                }
-                context.data.drugtype = drugdata;
-                next();
-              });
-          }).catch((error) => {
+  Medicinemaster.observe('loaded', function (context, next) {
+    let drugtypeId;
+    if (context.data.drugtype.includes('#')) {
+      drugtypeId = context.data.drugtype.split("#")[1];
+    }
+    else {
+      drugtypeId = context.data.drugtype;
+    }
+    bizNetworkConnection.connect(cardName)
+      .then((result) => {
+        bizNetworkConnection.getAssetRegistry('io.mefy.pharmacy.DrugType')
+          .then((assetRegistry) => {
+            return assetRegistry.get(drugtypeId);
+          }).then(function (drug) {
+            let drugdata = {
+              "$class": "io.mefy.pharmacy.DrugType",
+              "typeId": drug.typeId,
+              "type": drug.type,
+              "description": drug.description
+            }
+            context.data.drugtype = drugdata;
             next();
-            console.log('err'+error);
           });
+      }).catch((error) => {
+        next();
+        console.log('err' + error);
       });
-    
+  });
+
 
 };
