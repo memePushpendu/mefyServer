d8a8a3dd9b3f6d43ead28e35bb8674b7708ac5d6
Jyoti-meme <jyoti.mala@memeinfotech.com>
2018-08-20 11:19:46 +0530

    pharmacy post method made

--- common/models/pharmacy-item.js
+++ common/models/pharmacy-item.js
@@ -1,5 +1,61 @@
 'use strict';
+var app = require('../../server/server');
 
-module.exports = function(Pharmacyitem) {
+module.exports = function (Pharmacyitem) {
 
+    /**** PHARMACY ITEM CREATION  */
+    Pharmacyitem.beforeRemote('create', function (context, user, next) {
+        let date = new Date();
+        var milliSeconds = Date.parse(date);
+        //   PHARMACY EXISTENCE      
+        checkPharmcay(context.args.data.pharmacyId).then(function (result) {
+            // MEDICINE EXISTENCE
+            checkMedicine(context.args.data.medicineId).then(function (response) {
+                context.args.data.itemId = context.args.data.medicineId + '-' + milliSeconds + '-' + context.args.data.pharmacyId;
+                next();
+            })
+                .catch(function (err) {
+                    var err = new Error(err);
+                    err.statusCode = 404;
+                    next(err);
+                })
+        })
+            .catch(function (err) {
+                var err = new Error(err);
+                err.statusCode = 404;
+                next(err);
+            })
+    });
+
+
+    // CHECK PHARMACY EXISTENCE
+    function checkPharmcay(pharma) {
+        const Pharmacy = app.models.Pharmacy;
+        return new Promise((resolve, reject) => {
+            Pharmacy.exists(pharma, function (err, exists) {
+
+                if (exists) {
+                    resolve(true);
+                }
+                else {
+                    reject('PHAMRACY  WITH THIS ID DOESNOT EXISTS!');
+                }
+            })
+        })
+    }
+
+    // CHECK MEDICINE EXISTENCE
+    function checkMedicine(medicine) {
+        const MedicineMaster = app.models.MedicineMaster;
+        return new Promise((resolve, reject) => {
+            MedicineMaster.exists(medicine, function (err, exists) {
+                if (exists) {
+                    resolve(true);
+                }
+                else {
+                    reject('MEDICNE DOESNOT EXIST!')
+                }
+            })
+        })
+    }
 };
