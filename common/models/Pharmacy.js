9b3077fe3a6cdf0d348de5c623240baa02db5758
Pushpendu <contactpushpendu@gmail.com>
2018-08-23 09:57:57 +0530

    chnages

--- common/models/Pharmacy.js
+++ common/models/Pharmacy.js
@@ -69,7 +69,7 @@ module.exports = function (Pharmacy) {
   async function appendUsers(users) {
     let allusers = [];
     for (const user of users) {
-      // let duser = await processUser(user.toObject());
+      let duser = await processUser(user.toObject());
       // let sdata = { name: '', phoneNumber: '', role: '', $class: 'io.mefy.pharmacy.User' };
       // sdata.name = duser.name;
       console.log("prev user", user);
